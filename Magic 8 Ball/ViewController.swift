884bb08b1755c18c000b66a1979ed4ca8050a59a
Artem Krisanov <a.krisanov1@mail.ru>
2025-03-29 20:04:37 +0300

    Added new design

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,12 +12,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var magicBall: UIImageView!
     
+    @IBOutlet weak var question: UITextField!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
     @IBAction func magicBallButton(_ sender: UIButton) {
         
         magicBall.image = ballArray.randomElement()
+        question.text = ""
         
     }
     
